7695a4fbcc781b6cf302d8771f5f44eb9ede76bd
Sean Mongan <smongan1@gmail.com>
2026-03-18 11:55:58 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,1 @@
-const GITHUB_CONFIG = {
-  token1: 'gh_t1NKlSWx_76za9EfF2n8aysXZPDZ',
-  token2: 'iup_ANQWWnF0wvoMbDACQomnbyNYQoh',
-  token3: 'tba1BR0DK7oKX3AMzNwuxUvB2QFHBsb',
-  owner: 'smongan1',
-  repo:  'smongan1.github.io',
-  branch: 'messages'
-};
+const CONTACT_ENDPOINT = 'https://www.narrativetrace.com/submit-feedback';
