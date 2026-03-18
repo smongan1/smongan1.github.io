f5b20105a6f26228428261428ca224450a69b350
Sean Mongan <smongan1@gmail.com>
2026-03-17 23:06:26 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const GITHUB_CONFIG = {
-  token: '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',
+  token: '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',
   owner: 'smongan1',
   repo:  'smongan1.github.io',
   branch: 'messages'
