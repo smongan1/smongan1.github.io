5dfbbdd9156802905a3f2b20c0c65e3dedff9025
Sean Mongan <smongan1@gmail.com>
2026-03-17 22:59:18 -0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,6 @@
+const GITHUB_CONFIG = {
+  token: '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',
+  owner: 'smongan1',
+  repo:  'smongan1.github.io',
+  branch: 'messages'
+};
